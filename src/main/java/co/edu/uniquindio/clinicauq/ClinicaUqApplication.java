368dc83436ede31c6abdb8a9b2230cd0efd5c807
SebasUQ <juans.gonzalezm@uqvirtual.edu.co>
2026-07-02 17:51:44 -0500

    initial commit

---
+++ src/main/java/co/edu/uniquindio/clinicauq/ClinicaUqApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.clinicauq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaUqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClinicaUqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/edu/uniquindio/clinicauq/ClinicaUqApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.clinicauq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaUqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
