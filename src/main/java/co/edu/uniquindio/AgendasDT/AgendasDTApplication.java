2216c0a111ddce4a0af5ba4ac4f0f0289c9681cc
SebasUQ <juans.gonzalezm@uqvirtual.edu.co>
2026-09-09 15:01:56 -0500

    initial commit

---
+++ src/main/java/co/edu/uniquindio/AgendasDT/AgendasDTApplication.java
@@ -0,0 +1,17 @@
+package co.edu.uniquindio.AgendasDT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *  Clase principal, lanzador de todo el programa.
+ */
+
+@SpringBootApplication
+public class AgendasDTApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run(AgendasDTApplication.class, args);
+    }
+
+}
